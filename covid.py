d65fe8d07224cad1a282d4e4ea3b498f03e15ed9
L. Molnár <molnar.laci@outlook.com>
2020-04-01 10:37:27 +0200

    updates

--- covid.py
+++ covid.py
@@ -1,60 +1,118 @@
-import sys
+# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import datetime as datetime
 from bs4 import BeautifulSoup as bs
 import requests
+import sys
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
-DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
+now = datetime.datetime.today()
+ido = str(now.hour)+':'+str(now.minute)
+now = str(now.month)+'/'+str(now.day)+'/'+str(now.year)[2:]
 
-def load_data(the_file, the_country):
+
+DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+FILE_C = "time_series_covid19_confirmed_global.csv"
+FILE_D = "time_series_covid19_deaths_global.csv"
+FILE_R = "time_series_covid19_recovered_global.csv"
+country_table = {'United Kingdom': 'UK', 'United Arab Emirates': 'UAE', 'USA': 'US', 'Cote d\'Ivoire': 'Ivory Coast',
+                 'Congo (Brazzaville)': 'Congo', 'Saint Vincent and the Grenadines': 'St. Vincent Grenadines',
+                 'Korea, South': 'S. Korea', 'Taiwan*': 'Taiwan'}
+
+def load_data(the_file, country):
     data = pd.read_csv(the_file)
-    data.rename(columns={'Country/Region':'Country', 'Country_Region':'Country'}, inplace=True)
-    data = data[data['Country'] == the_country].fillna(0)
-    data = data.groupby(['Country']).sum()
-    data = data[['Confirmed', 'Recovered', 'Deaths']]
-    return (data.iloc[0,0], data.iloc[0,1], data.iloc[0,2]) if not data.empty else (0,0,0)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data.rename(columns={'country/region':'country', 'province/state':'state'}, inplace=True)
+    data.fillna(0, inplace=True)
+    data = data[data['country'] == country]
+    data = data[data.state == 0]
+    data = data.groupby(['country']).sum()
+    data = data.iloc[:,4:].T
+    data = data[(data.T != 0).any()]
+    data.rename(columns={data.columns[0]: 'Eset'}, inplace=True)
+    return data
+
+def str2int(s):
+    if s == ' ':
+        return 0
+    s = s.replace(',','')
+    return int(s) 
 
 def main(the_country):
-    START_DATE = datetime.date(2020,1,22)
-    TODAY_DATE = str(datetime.datetime.today())[:10]
-    the_data = dict()
-    print('Gathering data: ', end='')
-    while str(START_DATE) < TODAY_DATE:
-        the_date = START_DATE.strftime('%m-%d-%Y')
-        print(the_date, end=', ', flush=True)
-        file_name = f'{the_date}.csv'
-        data = load_data(DATA_URL + file_name, the_country)
-        if sum(data) > 0:
-            the_data[str(START_DATE)] = data 
-        START_DATE += datetime.timedelta(days=1)
-    print(' done!')
-    df = pd.DataFrame(the_data).T
-    df.columns=['Eset', 'Gyógyult', 'Halott']
 
+    df_d = load_data(DATA_URL + FILE_D, the_country) # Halottak
+    df_r = load_data(DATA_URL + FILE_R, the_country) # Gyógyultak
+    df_c = load_data(DATA_URL + FILE_C, the_country) # Esetek
+    df = pd.DataFrame()
+    df['Eset'] = df_c['Eset']
+    df['Gyógyult'] = df_r['Eset']
+    df['Halott'] = df_d['Eset']
+    dfT = df.T
+    
     if the_country == 'Hungary':
-            url='https://koronavirus.gov.hu/'
-            page = requests.get(url)
-            soup = bs(page.content, 'html.parser')
-            c = soup.find_all(class_ = 'number')
-            eset = int(c[0].text)
-            gyogyult = int(c[1].text)
-            halott = int(c[2].text)
-            dfT = df.T
-            dfT[TODAY_DATE] = [eset, gyogyult, halott]
-            df = dfT.T
+        url='https://koronavirus.gov.hu/'
+        page = requests.get(url)
+        soup = bs(page.content, 'html.parser')
+        c = soup.find_all(class_ = 'number')
+        eset = int(c[0].text)
+        gyogyult = int(c[1].text)
+        
+        page = requests.get(url+'/elhunytak')
+        soup = bs(page.content, 'html.parser')
+        c = soup.find(class_ = 'views-row-last')
+        c = c.find(class_ = 'views-field-field-elhunytak-sorszam')
+        halott = int(c.text)
+        dfT[now] = [eset, gyogyult, halott]
+    else:
+        if the_country in country_table:
+            the_country = country_table[the_country]
+        url = 'https://www.worldometers.info/coronavirus/#countries'
+        page = requests.get(url)
+        soup = bs(page.content, 'html.parser')
+        tbl = soup.find(id='main_table_countries_today')
+        tbl = tbl.findAll('tr')
+        eset = -1
+        for tr in tbl:
+            if the_country in tr.text:
+                tds = tr.findAll('td')
+                eset = str2int(tds[1].text)
+                halott = str2int(tds[3].text)
+                gyogyult = str2int(tds[5].text)
+                break
+        if eset > -1:
+            dfT[now] = [eset, gyogyult, halott]
+        else:
+            print(f'{the_country} is not on [page]({url}).')
+
+
+    dfT[now] = [eset, gyogyult, halott]
+    df = dfT.T
+
+    df.fillna(0, inplace= True)
+    df['Aktív'] = df['Eset']-df['Gyógyult'] - df['Halott']
+    df = df.astype(int)
+    
+    df = df.reset_index()
+    df.rename(columns = {'index': 'Dátum'}, inplace=True)
+    df['Dátum'] = pd.to_datetime(df['Dátum'])
+    df.set_index(['Dátum'], drop=True, inplace=True)
 
     df['Aktív'] = df['Eset']-(df['Gyógyult']+df['Halott'])
     df['Eset+'] = df['Eset'].shift(1)
+    df['Halott+'] = df['Halott'].shift(1)
     df.fillna(0, inplace=True)
     df['EsetD'] = abs(df['Eset'] - df['Eset+'])
-
-    plt.figure(figsize=(20,10), tight_layout=True)
-    ax = plt.subplot(211, frameon=False)
-
+    df['HalottD'] = abs(df['Halott'] - df['Halott+'])
+    df = df.astype(int)
+    
+    plt.figure(figsize=(20,30), tight_layout=True)
+    
+    ax = plt.subplot(311, frameon=True)
+    plt.grid(b=True, which='major', color='#666666', linestyle='-')
     plt.plot(df.index, df['Eset'], 'b-',     label=f"{df['Eset'].max():8} összesen")
     plt.plot(df.index, df['Gyógyult'], 'g--', label=f"{df['Gyógyult'].max():8} gyógyult")
     plt.plot(df.index, df['Halott'], 'r-',    label=f"{df['Halott'].max():8} halott")
@@ -68,18 +126,32 @@ def main(the_country):
     plt.legend(shadow=True, fontsize='small')
     plt.xticks(rotation='vertical')
 
-    bx = plt.subplot(212, frameon=True)
-    plt.bar(df.index, df['EsetD'], label='Napi növekmény', color='gray')
+    bx = plt.subplot(312, frameon=True)
+    plt.grid(axis='y', which='major', color='#666666', linestyle='-')
+    plt.bar(df.index, df['EsetD'], label='Eset', color='gray')
     plt.ylabel('Napi új eset')
+    plt.ylim(0,df['EsetD'].max() + 10)
     plt.xticks(rotation='vertical')
     plt.legend(shadow=True, fontsize='small')
 
     for i,j in df.EsetD.items():
-        bx.annotate(j, xy=(i, j + .3))
+        bx.annotate(j, xy=(i, j + 2))
+        
+    cx = plt.subplot(313, frameon=True)
+    plt.grid(axis='y', which='major', color='#666666', linestyle='-')
+    plt.ylabel('Napi elhalálozás')
+    plt.ylim(0,df['EsetD'].max() + 10)
+    plt.bar(df.index, df['HalottD'], label='Halott', color='red')
+    plt.xticks(rotation='vertical')
+    plt.legend(shadow=True, fontsize='small')
 
+    for i,j in df.HalottD.items():
+        cx.annotate(j, xy=(i, j + 0.05), color='red')
+        
     plt.savefig(f'{the_country.lower()}.png')
     print(f'Image saved as: {the_country.lower()}.png')
     # plt.show()
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
---
+++ st_corona.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime as datetime
+from bs4 import BeautifulSoup as bs
+import requests
+import sys
+
+now = datetime.datetime.today()
+ido = str(now.hour)+':'+str(now.minute)
+now = str(now.month)+'/'+str(now.day)+'/'+str(now.year)[2:]
+country_table = {'United Kingdom': 'UK', 'United Arab Emirates': 'UAE', 'USA': 'US', 'Cote d\'Ivoire': 'Ivory Coast',
+                 'Congo (Brazzaville)': 'Congo', 'Saint Vincent and the Grenadines': 'St. Vincent Grenadines',
+                 'Korea, South': 'S. Korea', 'Taiwan*': 'Taiwan'}
+
+DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+FILE_C = "time_series_covid19_confirmed_global.csv"
+FILE_D = "time_series_covid19_deaths_global.csv"
+FILE_R = "time_series_covid19_recovered_global.csv"
+
+st.title("Corona virus")
+st.markdown('The source data can be found [here](https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series)!')
+
+the_country = 'Hungary'
+
+@st.cache
+def load_data(the_file, country):
+    countries = []
+    data = pd.read_csv(the_file)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data.rename(columns={'country/region':'country', 'province/state':'state'}, inplace=True)
+    data.fillna(0, inplace=True)
+    countries.append(list(data['country']))
+    data = data[data['country'] == country]
+    if not data[data.state == 0].empty:
+        data = data[data.state == 0]
+    data = data.groupby(['country']).sum()
+    data = data.iloc[:,4:].T
+    data = data[(data.T != 0).any()]
+    data.rename(columns={data.columns[0]: 'Case'}, inplace=True)
+    return data, countries
+
+def str2int(s):
+    s = s.strip()
+    if s == '':
+        return 0
+    s = s.replace(',','')
+    return int(s)
+
+df_c, countries = load_data(DATA_URL + FILE_C, the_country) # Esetek
+countries = sorted(list(set(countries[0])))
+
+the_country = st.selectbox('Select country', countries)
+
+df_d,_ = load_data(DATA_URL + FILE_D, the_country) # Halottak
+df_r,_ = load_data(DATA_URL + FILE_R, the_country) # Gyógyultak
+df_c, countries = load_data(DATA_URL + FILE_C, the_country) # Esetek
+
+df = pd.DataFrame()
+df['Cases'] = df_c['Case']
+df['Recovered'] = df_r['Case']
+df['Dead'] = df_d['Case']
+
+dfT = df.T
+
+if the_country == 'Hungary':
+    url='https://koronavirus.gov.hu/'
+    page = requests.get(url)
+    soup = bs(page.content, 'html.parser')
+    c = soup.find_all(class_ = 'number')
+    eset = int(c[0].text)
+    gyogyult = int(c[1].text)
+    
+    page = requests.get(url+'/elhunytak')
+    soup = bs(page.content, 'html.parser')
+    c = soup.find(class_ = 'views-row-last')
+    c = c.find(class_ = 'views-field-field-elhunytak-sorszam')
+    halott = int(c.text)
+    
+    dfT[now] = [eset, gyogyult, halott]
+else:
+    if the_country in country_table:
+        the_country = country_table[the_country]
+
+    url = 'https://www.worldometers.info/coronavirus/#countries'
+    page = requests.get(url)
+    soup = bs(page.content, 'html.parser')
+    tbl = soup.find(id='main_table_countries_today')
+    tbl = tbl.findAll('tr')
+    eset = -1
+    for tr in tbl:
+        if the_country in tr.text:
+            tds = tr.findAll('td')
+            eset = str2int(tds[1].text)
+            halott = str2int(tds[3].text)
+            gyogyult = str2int(tds[5].text)
+            break
+    if eset > -1:
+        dfT[now] = [eset, gyogyult, halott]
+    else:
+        st.markdown(f'{the_country} is not on [page]({url}).')
+
+
+
+df = dfT.T
+
+df.fillna(0, inplace= True)
+df['Active'] = df['Cases']-df['Recovered'] - df['Dead']
+df = df.astype(int)
+
+df = df.reset_index()
+df.rename(columns = {'index': 'Date'}, inplace=True)
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index(['Date'], drop=True, inplace=True)
+
+df['Active'] = df['Cases']-(df['Recovered']+df['Dead'])
+df['Cases+'] = df['Cases'].shift(1)
+df['Dead+'] = df['Dead'].shift(1)
+df['Recovered+'] = df['Recovered'].shift(1)
+df.fillna(0, inplace=True)
+df['Cases/day'] = abs(df['Cases'] - df['Cases+'])
+df['Deads/day'] = abs(df['Dead'] - df['Dead+'])
+df['Recovered/day'] = abs(df['Recovered'] - df['Recovered+'])
+
+df.drop(['Cases+', 'Dead+', 'Recovered+'], axis=1, inplace=True)
+df = df.astype(int)
+
+m_cases = df['Cases'].iloc[-1]
+m_recovered = df['Recovered'].iloc[-1]
+m_dead = df['Dead'].iloc[-1]
+m_active = df['Active'].iloc[-1]
+
+st.header('The numbers')
+st.markdown(f'Cases: **{m_cases}** Recovered: **{m_recovered}** ({round(m_recovered/m_cases*100,2)}%) Deads: **{m_dead}** ({round(m_dead/m_cases*100,2)}%) Active: **{m_active}**')
+
+
+st.header('The datatable')
+st.dataframe(df)
+
+st.header('Cases, Active, Recovered and Deads')
+st.line_chart(df[['Cases', 'Active', 'Recovered', 'Dead']])
+
+st.header('Cases/day')
+st.bar_chart(df['Cases/day'])
+
+st.header('Recovered/day')
+st.bar_chart(df['Recovered/day'])
+
+st.header('Deads/day')
+st.bar_chart(df['Deads/day'])
